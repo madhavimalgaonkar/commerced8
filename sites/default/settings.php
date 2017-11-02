e83c4d9ef83ef77e63425f4639c836ebcbd47599
madhavimalgaonkar <madhavim2694@gmail.com>
2017-11-02 18:32:05 +0530

    Added Commerce module and other required modules and libraries [#151970616]

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,3 +23,15 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+$databases['default']['default'] = array (
+  'database' => 'commerced8',
+  'username' => 'root',
+  'password' => 'password',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'llyNeA36mUKHp71vW1445vpKDA8jNe0t6Eqtdk5PkWx-pMTfZ7S7HGj4XEoP6cu4fwyvyjIzsQ';
+$settings['install_profile'] = 'standard';
